fe5a5385e0ebeac551a3f0ae018989629f259ead
Nide17 <nidedrogba@gmail.com>
2023-01-04 11:35:50 +0200

    timestamp challenge

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,42 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+
+// API to get the date and convert into unix and utc
+app.get("/api/:stringDate?", function (req, res) {
+
+  var stringDate = req.params.stringDate;
+  var date;
+
+  if (stringDate == undefined) {
+    // Use the default today date
+    date = new Date();
+  }
+
+  else {
+    if (isNaN(stringDate)) {
+      date = new Date(stringDate);
+    } else {
+      date = new Date(parseInt(stringDate));
+    }
+  }
+
+  // If it cannot be parsed, return an error
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  }
+
+  // Else return the unix and utc
+  else {
+    var unix = date.getTime();
+    var utc = date.toUTCString();
+    res.json({ unix, utc });
+  }
+});
+
 
 
 
